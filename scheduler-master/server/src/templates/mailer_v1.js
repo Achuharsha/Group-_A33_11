08737c2380a01bd0e7cdc7de024280f6e0004eeb
Achuharsha <127080270+Achuharsha@users.noreply.github.com>
2023-05-12 21:48:03 +0530

    Delete scheduler-master directory

--- scheduler-master/server/src/templates/mailer_v1.js
+++
@@ -1,8 +0,0 @@
-module.exports = (link) => {
-	return `
-  <body style="font-size:16px">
-    <p>From scheduler app..</p>
-    <p>Reset your password <a href="${link}">here</a></p>
-  </body>
-  `;
-};
--- scheduler-master/tests/index.test.js
+++
@@ -1,3 +0,0 @@
-test('Fake test', () => {
-	expect(1 + 2).toBe(3);
-});
